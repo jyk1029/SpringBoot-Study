6067d10698f21fe3fb65855ea81dde323f1c555c
주영 <jyk102999@dsm.hs.kr>
2022-11-30 23:18:27 +0900

    ⚙️ :: 의존성 추가

--- Kopring/Dise_V2/build.gradle.kts
+++ Kopring/Dise_V2/build.gradle.kts
@@ -30,7 +30,10 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("mysql:mysql-connector-java")
+    implementation("io.jsonwebtoken:jjwt:0.9.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
