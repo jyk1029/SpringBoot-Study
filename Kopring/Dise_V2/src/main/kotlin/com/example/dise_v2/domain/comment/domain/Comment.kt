d63067af174ac138117ae80bd3d9ba6686eec227
주영 <jyk102999@dsm.hs.kr>
2022-12-21 14:30:40 +0900

    ⚡️ :: Comment 엔티티 생성

---
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/comment/domain/Comment.kt
@@ -0,0 +1,26 @@
+package com.example.dise_v2.domain.feed.domain
+
+import com.example.dise_v2.domain.user.domain.User
+import jakarta.persistence.*
+
+@Entity
+class Comment(
+    id: Long?,
+    content: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    var feed: Feed
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = id
+
+    @Column(length = 1000, nullable = false)
+    var content: String = content
+        protected set
+}
