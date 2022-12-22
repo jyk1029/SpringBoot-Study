c159c83e25fb4f48e1574e28cd1846972972a441
주영 <jyk102999@dsm.hs.kr>
2022-12-22 21:22:04 +0900

    ⚡️ :: 코드 정렬

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/error/GloblaExceptionFilter.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/error/GloblaExceptionFilter.kt
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GloblaExceptionFilter {
-
     @ExceptionHandler(CustomException::class)
-    fun customException (e : CustomException) : ResponseEntity<ErrorResponse> {
+    fun customException(e: CustomException): ResponseEntity<ErrorResponse> {
         return ResponseEntity(
             ErrorResponse.of(e),
             HttpStatusCode.valueOf(e.status)
