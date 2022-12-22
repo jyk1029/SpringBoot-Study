a4e826f761640e702670157a244c407adcd95ee0
주영 <jyk102999@dsm.hs.kr>
2022-12-22 21:20:19 +0900

    ⚙️ :: WebMvcConfig 설정

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/config/WebMvcConfig.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/config/WebMvcConfig.kt
@@ -1,4 +1,15 @@
 package com.example.dise_v2.global.config
 
-class WebMvcConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+    }
 }
