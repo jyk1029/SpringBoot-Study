803b075f1a46144a375ed1d1cca03f52844d95b4
주영 <jyk102999@dsm.hs.kr>
2022-12-23 09:17:33 +0900

    ⚡️ :: 물음표(nullable) 삭제

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/domain/repository/UserRepository.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/domain/repository/UserRepository.kt
@@ -3,7 +3,7 @@ package com.example.dise_v2.domain.user.domain.repository
 import com.example.dise_v2.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long?> {
+interface UserRepository : JpaRepository<User, Long> {
     fun findByAccountId(accountId: String): User
     fun existsByAccountId(accountd: String): Boolean
 }
