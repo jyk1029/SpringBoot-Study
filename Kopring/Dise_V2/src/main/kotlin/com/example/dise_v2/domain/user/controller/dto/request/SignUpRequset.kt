faf03d4da2672c0fc33ac8309b52fcff4481e182
주영 <jyk102999@dsm.hs.kr>
2022-12-25 22:02:51 +0900

    ⚡️ :: 정규식 다시 추가

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/controller/dto/request/SignUpRequset.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/controller/dto/request/SignUpRequset.kt
@@ -10,7 +10,10 @@ data class SignUpRequset(
     var accountId: String,
 
     @NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Size(min = 20, max = 20)
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!/?@])[a-zA-Z0-9!/" +
+            "?@]{8,20}$",
+        message = "password는 소문자, 숫자, 특수문자가 포함되어야 하며 8자 이상 20자 이하여야 합니다."
+    )
     var password: String,
 
     @NotBlank(message = "name은 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
