7cde7f51fe15eed82ce3803ea53eb7db1302112f
주영 <jyk102999@dsm.hs.kr>
2022-12-27 18:59:52 +0900

    🐛 :: Field 추가

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/controller/dto/request/SignUpRequset.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/controller/dto/request/SignUpRequset.kt
@@ -5,18 +5,18 @@ import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
 data class SignUpRequset(
-    @NotBlank(message = "account_id는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Size(min = 8, max = 20, message = "account_id는 8자 이상 20자 이하여야 합니다.")
+    @field:NotBlank(message = "account_id는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @field:Size(min = 8, max = 20, message = "account_id는 8자 이상 20자 이하여야 합니다.")
     var accountId: String,
 
-    @NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!/?@])[a-zA-Z0-9!/" +
+    @field:NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @field:Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!/?@])[a-zA-Z0-9!/" +
             "?@]{8,20}$",
         message = "password는 소문자, 숫자, 특수문자가 포함되어야 하며 8자 이상 20자 이하여야 합니다."
     )
     var password: String,
 
-    @NotBlank(message = "name은 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Size(max = 4, message = "name은 4자 이하여야 합니다.")
+    @field:NotBlank(message = "name은 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @field:Size(max = 4, message = "name은 4자 이하여야 합니다.")
     var name: String
 )
