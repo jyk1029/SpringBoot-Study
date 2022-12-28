13072c76d21e0f1b57e9b6b9e363b3297c9504ab
주영 <jyk102999@dsm.hs.kr>
2022-12-28 14:18:34 +0900

    ⚡️ :: Facads 패턴 적용

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/service/SignUpService.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/service/SignUpService.kt
@@ -4,16 +4,18 @@ import com.example.dise_v2.domain.user.controller.dto.request.SignUpRequset
 import com.example.dise_v2.domain.user.domain.User
 import com.example.dise_v2.domain.user.domain.repository.UserRepository
 import com.example.dise_v2.domain.user.exception.AlreadyUserExistException
+import com.example.dise_v2.domain.user.facade.UserFacade
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class SignUpService(
     private val userRepository: UserRepository,
+    private val userFacade: UserFacade,
     private val passwordEncoder: PasswordEncoder
 ) {
     fun execute(request: SignUpRequset) {
-        if (userRepository.existsByAccountId(request.accountId)) {
+        if (userFacade.checkUserExist(request.accountId)) {
             throw AlreadyUserExistException.EXCEPTION
         }
 
