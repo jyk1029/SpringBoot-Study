64985d1db20abceaeae09507f77e03afd40714fc
주영 <jyk102999@dsm.hs.kr>
2022-12-28 17:35:46 +0900

    🔥 :: 코드 정렬

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/controller/dto/response/TokenResponse.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/user/controller/dto/response/TokenResponse.kt
@@ -1,7 +1,5 @@
 package com.example.dise_v2.domain.user.controller.dto.response
 
-class TokenResponse (
-    accessToken: String
-) {
-    private val accessToken: String = accessToken
-}
+data class TokenResponse (
+    val accessToken: String
+)
