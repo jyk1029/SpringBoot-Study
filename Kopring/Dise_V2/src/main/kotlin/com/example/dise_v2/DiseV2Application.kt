51b48cc5ab4c2d171072b7e928b1c148a5d80bea
주영 <jyk102999@dsm.hs.kr>
2023-01-01 23:20:36 +0900

    ⚡️ :: @EnableConfigurationProperties 추가

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/DiseV2Application.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/DiseV2Application.kt
@@ -1,10 +1,13 @@
 package com.example.dise_v2
 
+import com.example.dise_v2.global.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @ConfigurationPropertiesScan
+@EnableConfigurationProperties(JwtProperties::class)
 @SpringBootApplication
 class DiseV2Application
 
