c285ea246eb39ad5fef61831efeb81e371e81826
주영 <jyk102999@dsm.hs.kr>
2023-01-01 23:32:53 +0900

    ⚡️ :: JwtTokenFilter로 파일 이름 변경

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/security/FilterConfig.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/security/FilterConfig.kt
@@ -1,7 +1,7 @@
 package com.example.dise_v2.global.security
 
 import com.example.dise_v2.global.error.ExceptionFilter
-import com.example.dise_v2.global.security.jwt.JwtFilter
+import com.example.dise_v2.global.security.jwt.JwtTokenFilter
 import com.example.dise_v2.global.security.jwt.JwtTokenProvider
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
@@ -15,11 +15,11 @@ class FilterConfig(
 ) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
     override fun configure(builder: HttpSecurity) {
         builder.addFilterBefore(
-            JwtFilter(jwtTokenProvider),
+            JwtTokenFilter(jwtTokenProvider),
             UsernamePasswordAuthenticationFilter::class.java
         )
         builder.addFilterBefore(
-            ExceptionFilter(objectMapper), JwtFilter::class.java
+            ExceptionFilter(objectMapper), JwtTokenFilter::class.java
         )
     }
 }
--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/security/jwt/JwtTokenFilter.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/security/jwt/JwtTokenFilter.kt
@@ -7,7 +7,7 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
-class JwtFilter(
+class JwtTokenFilter(
     private val jwtTokenProvider: JwtTokenProvider
 ) : OncePerRequestFilter() {
     override fun doFilterInternal(
