5cf80ed2ec2676e15dc9970cf741d30c1fb24bd7
주영 <jyk102999@dsm.hs.kr>
2023-01-01 23:18:48 +0900

    ⚡️ :: Redis 의존성 추가

--- Kopring/Dise_V2/build.gradle.kts
+++ Kopring/Dise_V2/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
 
 tasks.withType<KotlinCompile> {
