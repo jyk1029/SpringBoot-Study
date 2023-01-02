1d8760a6531ecb190e794d6be5d5379fc9807176
주영 <jyk102999@dsm.hs.kr>
2023-01-02 10:31:18 +0900

    ⚡️ :: @ConstructorBinding 추가

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/security/jwt/JwtProperties.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/security/jwt/JwtProperties.kt
@@ -1,10 +1,12 @@
 package com.example.dise_v2.global.security.jwt
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "jwt")
 data class JwtProperties(
     var secret: String,
     val accessExp: Long,
-    val refreshExp: Long,
+    val refreshExp: Long
 )
