d1177491fc80d3e13ce826ab2fdc92350d6e3243
주영 <jyk102999@dsm.hs.kr>
2023-01-06 10:56:17 +0900

    ⚡️ :: 파일 이름 변경

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/error/GloblaExceptionHandler.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/global/error/GloblaExceptionHandler.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class GloblaExceptionFilter {
+class GloblaExceptionHandler {
     @ExceptionHandler(CustomException::class)
     fun customException(e: CustomException): ResponseEntity<ErrorResponse> {
         return ResponseEntity(
