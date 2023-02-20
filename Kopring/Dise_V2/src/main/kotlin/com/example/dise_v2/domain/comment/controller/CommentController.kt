407d7d84b5acb27d8cd6d9bee7a7aad78f2c21d9
주영 <jyk102999@dsm.hs.kr>
2023-02-20 23:40:04 +0900

    ⚡️ :: 댓글 수정 Controller 추가

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/comment/controller/CommentController.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/comment/controller/CommentController.kt
@@ -1,10 +1,13 @@
 package com.example.dise_v2.domain.comment.controller
 
 import com.example.dise_v2.domain.comment.controller.dto.request.CreateCommentRequest
+import com.example.dise_v2.domain.comment.controller.dto.request.UpdateCommentRequest
 import com.example.dise_v2.domain.comment.service.CreateCommentService
+import com.example.dise_v2.domain.comment.service.UpdateCommentService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -14,11 +17,18 @@ import javax.validation.Valid
 @RequestMapping("/comment")
 @RestController
 class CommentController(
-    private val createCommentService: CreateCommentService
+    private val createCommentService: CreateCommentService,
+    private val updateCommentService: UpdateCommentService
 ) {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{feed-id}")
     fun createComment(@PathVariable("feed-id") feedId: Long, @RequestBody @Valid request: CreateCommentRequest) {
         createCommentService.execute(feedId, request)
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{comment-id}")
+    fun modifyComment(@PathVariable("comment-id") commentId: Long, @RequestBody @Valid request: UpdateCommentRequest) {
+        updateCommentService.execute(commentId, request)
     }
 }
