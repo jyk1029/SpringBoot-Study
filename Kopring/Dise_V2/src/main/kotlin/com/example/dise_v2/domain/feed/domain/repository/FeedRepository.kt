2188b6e06ac89fb831bcafddd9b8293e27569894
주영 <jyk102999@dsm.hs.kr>
2023-02-20 21:25:41 +0900

    🔥 :: 코드 정렬

--- Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/feed/domain/repository/FeedRepository.kt
+++ Kopring/Dise_V2/src/main/kotlin/com/example/dise_v2/domain/feed/domain/repository/FeedRepository.kt
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository
 @Repository
 interface FeedRepository : JpaRepository<Feed, Long> {
     fun findAllByOrderByIdDesc(): List<Feed>
-    fun findByCategoryOrderByCreatedAtDesc(category: Category) : List<Feed>
+    fun findByCategoryOrderByCreatedAtDesc(category: Category): List<Feed>
     fun findAllByUserOrderByCreatedAtDesc(user: User): List<Feed>
 }
