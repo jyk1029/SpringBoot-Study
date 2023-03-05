8d0ef419ee607026ff42f451b2fc8fc148307cf9
주영 <jyk102999@dsm.hs.kr>
2023-03-05 20:46:20 +0900

    🗑 :: 안 쓰는 import 삭제

--- Board/src/main/java/com/example/board/BoardApplication.java
+++ Board/src/main/java/com/example/board/BoardApplication.java
@@ -3,7 +3,6 @@ package com.example.board;
 import com.example.board.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties(JwtProperties.class)
