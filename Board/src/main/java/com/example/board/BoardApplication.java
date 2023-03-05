52440c5f02fa1eb2bde638888704311f8ddef67b
주영 <jyk102999@dsm.hs.kr>
2023-03-05 18:46:55 +0900

    ♻️ :: @EnableConfigurationProperies로 변경

--- Board/src/main/java/com/example/board/BoardApplication.java
+++ Board/src/main/java/com/example/board/BoardApplication.java
@@ -1,10 +1,12 @@
 package com.example.board;
 
+import com.example.board.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@ConfigurationPropertiesScan
+@EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
 public class BoardApplication {
 
